a1503b9dadf275dd0b45cc712f2b8b4850eb5678
Zhaky <cybergolem@gmail.com>
2025-09-01 15:44:07 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("button-menu_dashboard").onclick = function() {
+    window.location.href = "index.htm";
+}
+
+document.getElementById("button-menu_plan").onclick = function() {
+    window.location.href = "plan.htm";
+}
+
+document.getElementById("button-menu_recipes").onclick = function() {
+    window.location.href = "recipe.htm";
+}
+
+document.getElementById("button-menu_storage").onclick = function() {
+    window.location.href = "storage.htm";
+}
