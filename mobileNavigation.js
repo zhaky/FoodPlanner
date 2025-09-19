9ebc4bb305a2b14d406912edf51a1889eb3d0b20
Zhaky <cybergolem@gmail.com>
2025-09-19 12:11:53 +0200

    Fixed mobileNavigation.js. Need to test if it works on mobile, as it seems like it might be confused about the direction being swiped

--- mobileNavigation.js
+++ mobileNavigation.js
@@ -25,6 +25,7 @@ hammer.on('swipeleft', function() {
     const pageIndex = getCurrentPageIndex();
     // If user swipes left on first page, go to last page
     const previousPageIndex = (pageIndex === 0) ? pages.length - 1 : pageIndex - 1;
+    //console.log("Swiping left to: " + pages[previousPageIndex]);
     window.location.href = pages[previousPageIndex];
 }
 );
@@ -33,5 +34,6 @@ hammer.on('swiperight', function() {
     const pageIndex = getCurrentPageIndex();
     // If user swipes right on last page, go to first page
     const nextPageIndex = (pageIndex === pages.length - 1) ? 0 : pageIndex + 1;
-    window.location.href = pages
+    //console.log("Swiping right to: " + pages[nextPageIndex]);
+    window.location.href = pages[nextPageIndex];
 });
